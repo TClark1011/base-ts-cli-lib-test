dd0bc80297d0b3f507eccf244046ed88113e302d
Thomas Clark <thomassiclark@gmail.com>
2025-05-06 14:39:58 +1000

    feat(lib): `isNot` function

--- packages/lib/src/index.ts
+++ packages/lib/src/index.ts
@@ -1,14 +1,14 @@
-export const reverse = <T>(arr: T[]): T[] =>
-	[...arr].reverse();
+export const reverse = <T>(arr: T[]): T[] => [...arr].reverse();
 
-export const sort = <T>(arr: T[], comparator: (a: T, b: T) => number): T[] =>
-	[...arr].sort(comparator);
+export const sort = <T>(arr: T[], comparator: (a: T, b: T) => number): T[] => [...arr].sort(comparator);
 
-export const isString = (value: unknown): value is string =>
-	typeof value === 'string';
+export const isString = (value: unknown): value is string => typeof value === 'string';
 
-export const isNumber = (value: unknown): value is number =>
-	typeof value === 'number';
+export const isNumber = (value: unknown): value is number => typeof value === 'number';
 
-export const isBoolean = (value: unknown): value is boolean =>
-	typeof value === 'boolean';
+export const isBoolean = (value: unknown): value is boolean => typeof value === 'boolean';
+
+export const isNot = <Base, T extends Base>(
+	value: Base,
+	guard: (value: Base) => value is T
+): value is Exclude<Base, T> => !guard(value);
