41fc37175267f3517564b37dbb70b7bc521604a2
Thomas Clark <thomassiclark@gmail.com>
2025-05-07 18:31:22 +1000

    ci(lib): initial unit testing setup

---
+++ packages/lib/src/index.spec.ts
@@ -0,0 +1,10 @@
+import { it } from "node:test";
+import { reverse } from './array/reverse';
+import assert from "node:assert";
+
+it("does not mutate the original array", () => {
+	const input = [1, 2, 3];
+	reverse(input);
+
+	assert.deepEqual(input, [1, 2, 3]);
+})
