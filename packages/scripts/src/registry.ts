4dc1fd4edd715b72269234f7ffa64a57f203aed9
Thomas Clark <thomassiclark@gmail.com>
2025-05-07 16:40:53 +1000

    ci(scripts): setup scripts package, implemented registry generation script

---
+++ packages/scripts/src/registry.ts
@@ -0,0 +1,28 @@
+/**
+ * This script acts as a type-safe way to write the registry file
+ */
+
+import { UtilityFunctionRegistry } from "@bt/types";
+import path from "path";
+import fs from "fs";
+
+// WRITE REGISTRY HERE
+const REGISTRY: UtilityFunctionRegistry = [
+	{
+		name: 'reverse',
+		group: 'array',
+		ts: 'packages/lib/src/array/reverse.ts',
+		js: 'packages/lib/dist/array/reverse.js',
+	}
+]
+
+// Then we write it to the registry.json file
+
+const filePath = path.join(process.cwd(), '../../registry.json');
+fs.writeFile(filePath, JSON.stringify(REGISTRY), (err) => {
+	if (err) {
+		console.error('Error writing file:', err);
+	} else {
+		console.log('File has been written successfully');
+	}
+});
